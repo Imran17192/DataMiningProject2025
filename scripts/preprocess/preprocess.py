e01a28aa75c3f520c3a29037b8fd782bed6e85f4
Imran17192 <narmi3002@gmail.com>
2025-06-13 15:58:03 +0200

    feat: Romev Outliers
    - Added funciton to clean outliers
    - Function takes interquartile Range(IQR) with upper and lower bound to filter the most importantn values and delete rest of the values, which are only few percent of whole dataframe

--- scripts/preprocess/preprocess.py
+++ scripts/preprocess/preprocess.py
@@ -24,11 +24,65 @@ def load_data():
     return df_x, df_ds1
 
 
-def data_fram_information(dfs):
-  return 0
+def inspect_dataframe(dfs, name):
+    print("------------------------------------------------------", name, "------------------------------------------------------")
+    i = 1
+    for df in dfs:
+        print(i,"." , name, "dataframe\n")
+        # first 5 lines of the dataframe
+        print("head:")
+        print(df.head())
+        # last 5 lines of the dataframe
+        print("tail:")
+        print(df.tail())
+        # class of the object, index range so how many line, column rang so how many columns
+        # detailed descr of the colums, so amount, column name, how many lines arent null, datatype of datapoint
+        # shows memory usage
+        print("info:")
+        print(df.info())
+        # gives us for each column count of lines, mean, std, min, ...
+        print("describe:")
+        print(df.describe())
+        i += 1
+
+
+def remove_oultiers(df_x):
+    df_cleaned = []
+    for df in df_x:
+        Q1 = df.quantile(0.25)
+        Q3 = df.quantile(0.75)
+
+        IQR = Q3 - Q1
+
+        lower_limit = Q1 - 1.5 * IQR
+        upper_limit = Q3 + 1.5 * IQR
+
+        df_no_outliers = df[((df >= lower_limit) & (df <= upper_limit)).all(axis=1)]
+        df_cleaned.append(df_no_outliers)
+
+    return df_cleaned
+
 
 if __name__ == "__main__":
     df_x, df_ds1 = load_data()
-    data_fram_information(df_x)
-    data_fram_information(df_ds1)
+
+    inspect_dataframe(df_x, "x_data_frame")
+    df_x_clean = remove_oultiers(df_x)
+    inspect_dataframe(df_x_clean, "x_data_frame removed ouliers")
+
+    inspect_dataframe(df_ds1, "ds_data_frame")
+    df_ds1 = remove_oultiers(df_ds1)
+    inspect_dataframe(df_ds1, "ds_data_frame removed ouliers")
+
+
+
+    # TODO df shapes
+    # TODO df cleaning
+    # TODO df scaling ana normalizing
+    # TODO df visualizations
+    # TODO filter noise
+    # TODO heatmaps
+    # TODO PCA
+    # TODO Quantifying
+    # TODO Automated Branch and bound
 
