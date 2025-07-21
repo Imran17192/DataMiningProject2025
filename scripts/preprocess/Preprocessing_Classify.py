9e070980e346401d163352980a36cff0da5380a2
Marvin <marvinfuerstenberg@t-online.de>
2025-07-21 23:46:14 +0200

    Optimize imports

--- scripts/preprocess/Preprocessing_Classify.py
+++ scripts/preprocess/Preprocessing_Classify.py
@@ -1,11 +1,13 @@
 import matplotlib
+
 matplotlib.use('TkAgg')
+
 import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
+import pandas as pd
+
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
-import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 
 
